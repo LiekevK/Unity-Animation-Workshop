a0166d4a69d41400c887bd8989087c3736791071
LiekevanKempen <Liekevankempen60@gmail.com>
2026-03-26 11:11:01 +0100

    removed working parts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,27 +4,25 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private CharacterController characterController;
-    private Animator animator;
     private Vector2 inputVector;
     [SerializeField] private float speed = 2f;
     private int speedModifier = 1;
-    private bool isTorchEquipped;
     
     [SerializeField] private GameObject torch;
     
-    // Hash
-    private int velocityXHash;
-    private int velocityYHash;
-    private int torchHash;
+    // TODO: Place hash variables here
+    
+    // TODO: Add Animator
+    
+    // TODO: Add bool for equipping the torch
     
     void Start()
     {
         characterController = GetComponent<CharacterController>();
-        animator = GetComponent<Animator>();
         
-        velocityXHash = Animator.StringToHash("VelocityX");
-        velocityYHash = Animator.StringToHash("VelocityY");
-        torchHash = Animator.StringToHash("IsTorchEquipped");
+        // TODO: Get Animator component
+        
+        // TODO: set the Hash variables with StringToHash on animator
     }
     
     void Update()
@@ -33,8 +31,8 @@ public class PlayerController : MonoBehaviour
         characterController.Move(speed * speedModifier * movement * Time.deltaTime);
         characterController.Move(Physics.gravity * Time.deltaTime);
         
-        animator.SetFloat(velocityXHash, inputVector.x * speedModifier);
-        animator.SetFloat(velocityYHash, inputVector.y * speedModifier);
+        // TODO: Update animator parameters
+        
     }
 
     void OnMove(InputValue value)
@@ -49,12 +47,10 @@ public class PlayerController : MonoBehaviour
 
     void OnAttack(InputValue value)
     {
-        isTorchEquipped = !isTorchEquipped;
-        animator.SetBool(torchHash, isTorchEquipped);
-        
+        // TODO
     }
     public void ToggleTorch()
     {
-       torch.SetActive(!torch.activeSelf);
+       // TODO
     }
 }
